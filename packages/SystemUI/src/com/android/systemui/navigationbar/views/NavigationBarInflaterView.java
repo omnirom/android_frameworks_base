81349ffaf83d062658caadcf3c533d645d117d5f
micky387 <mickaelsaibi@free.fr>
2025-05-23 23:05:15 -0400

    Revert "[1/2] SystemUI: add option to hide gesture navigation and size config handle"
    This reverts commit dbabbfefae3ae8d7b67167be0b0974ff8191df40.
    
    Moved to Launcher3 taskbar
    Change-Id: I203fc64712f83bebe8ad9a2dba4c891d72d6f841
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- packages/SystemUI/src/com/android/systemui/navigationbar/views/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/navigationbar/views/NavigationBarInflaterView.java
@@ -164,21 +164,13 @@ public class NavigationBarInflaterView extends FrameLayout
     }
 
     protected String getDefaultLayout() {
-        if (QuickStepContract.isGesturalMode(mNavBarMode)) {
-            String navbarLayout = getContext().getString(showDpadArrowKeys()
-                    ? R.string.config_navBarLayoutHandleArrows
-                    : R.string.config_navBarLayoutHandle);
-            if (hideGestureHandle()) {
-                return navbarLayout.replace(HOME_HANDLE, NAVSPACE);
-            } else {
-                return navbarLayout;
-            }
-        } else {
-            final int defaultResource = mOverviewProxyService.shouldShowSwipeUpUI()
-                            ? R.string.config_navBarLayoutQuickstep
-                            : R.string.config_navBarLayout;
-            return getContext().getString(defaultResource);
-        }
+        final int defaultResource = QuickStepContract.isGesturalMode(mNavBarMode)
+                        ? (showDpadArrowKeys() ? R.string.config_navBarLayoutHandleArrows
+                        : R.string.config_navBarLayoutHandle)
+                : mOverviewProxyService.shouldShowSwipeUpUI()
+                        ? R.string.config_navBarLayoutQuickstep
+                        : R.string.config_navBarLayout;
+        return getContext().getString(defaultResource);
     }
 
     private void onNavigationModeChanged(int mode) {
@@ -197,10 +189,6 @@ public class NavigationBarInflaterView extends FrameLayout
         super.onAttachedToWindow();
         Dependency.get(OmniSettingsService.class).addIntObserver(this,
             OmniSettings.OMNI_NAVIGATION_BAR_ARROW_KEYS);
-        Dependency.get(OmniSettingsService.class).addIntObserver(this,
-            OmniSettings.OMNI_GESTURE_HANDLE_HIDE);
-        Dependency.get(OmniSettingsService.class).addIntObserver(this, 
-            OmniSettings.OMNI_GESTURE_HANDLE_SMALL);
     }
 
     public void onLikelyDefaultLayoutChange() {
@@ -210,12 +198,6 @@ public class NavigationBarInflaterView extends FrameLayout
             clearViews();
             inflateLayout(newValue);
         }
-    }
-
-    private void onForceDefaultLayoutChange() {
-        final String newValue = getDefaultLayout();
-        clearViews();
-        inflateLayout(newValue);
     }
 
     public void setButtonDispatchers(SparseArray<ButtonDispatcher> buttonDispatchers) {
@@ -436,7 +418,7 @@ public class NavigationBarInflaterView extends FrameLayout
         } else if (CONTEXTUAL.equals(button)) {
             v = inflater.inflate(R.layout.contextual, parent, false);
         } else if (HOME_HANDLE.equals(button)) {
-            v = inflater.inflate(isSmallGestureHandle() ? R.layout.home_handle_small : R.layout.home_handle, parent, false);
+            v = inflater.inflate(R.layout.home_handle, parent, false);
         } else if (IME_SWITCHER.equals(button)) {
             v = inflater.inflate(R.layout.ime_switcher, parent, false);
         } else if (button.startsWith(KEY)) {
@@ -538,19 +520,9 @@ public class NavigationBarInflaterView extends FrameLayout
                 OmniSettings.OMNI_NAVIGATION_BAR_ARROW_KEYS, 0, UserHandle.USER_CURRENT) != 0;
     }
 
-    private boolean hideGestureHandle() {
-        return Settings.System.getIntForUser(getContext().getContentResolver(),
-                OmniSettings.OMNI_GESTURE_HANDLE_HIDE, 0, UserHandle.USER_CURRENT) != 0;
-    }
-
-    private boolean isSmallGestureHandle() {
-        return Settings.System.getIntForUser(getContext().getContentResolver(),
-                OmniSettings.OMNI_GESTURE_HANDLE_SMALL, 0, UserHandle.USER_CURRENT) != 0;
-    }
-
     @Override
     public void onIntSettingChanged(String key, Integer newValue) {
-        onForceDefaultLayoutChange();
+        onLikelyDefaultLayoutChange();
     }
 
     public void dump(PrintWriter pw) {
