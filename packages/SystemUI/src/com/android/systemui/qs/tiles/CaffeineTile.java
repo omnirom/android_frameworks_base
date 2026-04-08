db6b9ded802340952a99b88d02692e5331d1bb75
micky387 <mickaelsaibi@free.fr>
2026-04-07 20:24:25 -0400

    SystemUi: tiles: Preload icon drawable for our custom Modes tile
    reference ce8bee1b93af62a57097ca60c36265488a1d9371
    
    Change-Id: I18c259223a767b2517d1a31cd570566dfdd9254f
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/CaffeineTile.java
@@ -52,7 +52,8 @@ import javax.inject.Inject;
 public class CaffeineTile extends QSTileImpl<BooleanState> {
     public static final String TILE_SPEC = "caffeine";
 
-    private final Icon mIcon = ResourceIcon.get(R.drawable.ic_qs_caffeine);
+    @Nullable
+    private Icon mIcon = null;
 
     private final PowerManager.WakeLock mWakeLock;
     private int mSecondsRemaining;
@@ -215,6 +216,9 @@ public class CaffeineTile extends QSTileImpl<BooleanState> {
     @Override
     protected void handleUpdateState(BooleanState state, Object arg) {
         state.value = mWakeLock.isHeld();
+        if (mIcon == null) {
+            mIcon = maybeLoadResourceIcon(R.drawable.ic_qs_caffeine);
+        }
         state.icon = mIcon;
         state.label = mContext.getString(R.string.quick_settings_caffeine_label);
         if (state.value) {
